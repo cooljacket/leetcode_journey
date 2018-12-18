8b433a54cce38c50e4f75dbb719313f4d8373e0f
jacket <jacket@jacketdeMacBook-Pro.local>
2018-12-18 21:37:52 +0800

    构建过程中增加头文件查找目录

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,5 +1,5 @@
-filename=${1:0:${#1}-4}
+filename=${1:6:${#1}-10}
 echo "Start to build "$filename
-g++ $1 -std=c++11 -o out
+g++ $1 -std=c++11 -I. -o out
 echo "Done build "$filename", start to run"
 ./out < test_cases/${filename}.txt
